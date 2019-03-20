4a81475c1cebedb3f4c6aeb9a63938b5734cbc1a
Brian Quiroz <r229q057@ku.edu>
2019-03-19 21:33:19 -0500

    commit

--- mlr.py
+++ mlr.py
@@ -1,14 +1,18 @@
 import disnames
-import getdata
+from getdata import rows
 
-rows = getdata.rows
-
-print(getdata.analysis)
-[print(i + ":", str(j)) for i, j in getdata.analysis.items()]
+# print(getdata.analysis)
+# [print(i + ":", str(j)) for i, j in getdata.analysis.items()]
 # test = disnames.consclusters(["Katie", "Jamie", "Ben", "Joe", "Alphonse"])
 # [print(i + ":", str(j)) for i, j in test.items()]
 
-anaysis = 
+states = [i.state for i in rows];
+data = disnames.consclusters([i.name for i in rows]);
+arr = [i.name for i in rows];
+arr2 = [i.year for i in rows];
 
-for i in rows:
-    analysis[i] = i.year
+print(data)
+[print(i + ":", str(j)) for i, j in data.items()]
+
+print(arr)
+print(arr2)
