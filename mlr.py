95bcea141621d609bd462ac843c11497ea5a8d0b
Brian Quiroz <r229q057@ku.edu>
2019-03-19 21:05:04 -0500

    asdfdsf

--- mlr.py
+++ mlr.py
@@ -1,1 +1,14 @@
+import disnames
 import getdata
+
+rows = getdata.rows
+
+print(getdata.analysis)
+[print(i + ":", str(j)) for i, j in getdata.analysis.items()]
+# test = disnames.consclusters(["Katie", "Jamie", "Ben", "Joe", "Alphonse"])
+# [print(i + ":", str(j)) for i, j in test.items()]
+
+anaysis = 
+
+for i in rows:
+    analysis[i] = i.year
