3bf77fb33388a42672c448250ede2e40a6e0c224
Brian Quiroz <r229q057@ku.edu>
2019-03-20 17:48:42 -0500

    MLR WORKINGG

--- mlr.py
+++ mlr.py
@@ -1,15 +1,8 @@
-from getdata import rows
-import disnames
-import dictionaries
-
 from pandas import DataFrame
 import matplotlib.pyplot as plt
-
+import numpy as np
 from sklearn import linear_model
 import statsmodels.api as sm
-
-gendersDict = dictionaries.genders
-statesDict = dictionaries.states
 
 def plotScatter(x, y, title, xlabel, ylabel):
     plt.scatter(x, y, color='red')
@@ -19,79 +12,44 @@ def plotScatter(x, y, title, xlabel, ylabel):
     plt.grid(True)
     plt.show()
 
-#-------------------------------------------- Data --------------------------------------------#
-
-#NOTE: Treating 'occurrences' as the dependent variable and everything else as independent variables
+#-------------------------------------------- Reading Data --------------------------------------------#
 
 data = {}
 
-nameclusters = disnames.consclusters([i.name for i in rows])
+file = open('../KS_new.csv')
+rows = file.read().splitlines()
+file.close()
+genders = []
+years = []
+lengths = []
 
-names = [i.name for i in rows]
-for i in range(0, len(names)):
-    names[i] = i;
-
-data['Name'] = names
-
-years = [i.year for i in rows]
-data['Year'] = years
-
-genders = [i.gender for i in rows]
-for i in (range(0, len(genders))):
-    genders[i] = gendersDict[genders[i]]
-data['Gender'] = genders
-
-states = [i.state for i in rows]
-# for i in (range(0, len(states))):
-#     states[i] = statesDict[states[i]]
-
-statecategories = []
-for i in (range(0, len(statesDict))):
-        statecategories.append([])
-
-for state in states:
-    for j in range(0, len(statesDict)):
-        if j == statesDict[state]:
-            statecategories[j].append(1);
+for i in range(0, len(rows)):
+    col = rows[i].split(',')
+    for j in range(0, len(col)):
+        if j == 0:
+            genders.append(col[j])
+        elif j == 1:
+            years.append(col[j])
         else:
-            statecategories[j].append(0);
+            lengths.append(col[j])
 
-i = 0
-for state in statesDict:
-    data[state] = statecategories[i]
-    i = i + 1
-
-occurrences = [i.number for i in rows]
-data['Occurrence'] = occurrences
+data['Gender'] = genders
+data['Year'] = years
+data['Length'] = lengths
 
 df = DataFrame(data,columns=[i for i in data])
 
-
-#-------------------------------------------- Printing (for debugging purposes) --------------------------------------------#
-[print(i + ":", str(j)) for i, j in nameclusters.items()]
-print(data)
-
 #-------------------------------------------- Plotting --------------------------------------------#
 
-#Plot Name vs Occurrence
-plotScatter(df['Name'], df['Occurrence'], 'Scatter Plot', 'Name', 'Occurrence')
+#Plot Gender vs Length
+plotScatter(df['Gender'], df['Length'], 'Scatter Plot', 'Gender', 'Length')
 
-#Plot Year vs Occurrence
-plotScatter(df['Year'], df['Occurrence'], 'Scatter Plot', 'Year', 'Occurrence')
-
-#Plot Gender vs Occurrence
-plotScatter(df['Gender'], df['Occurrence'], 'Scatter Plot', 'Gender', 'Occurrence')
-
-#Plot Each State vs Occurrence
-#i = 0
-# for state in statesDict:
-#     plotScatter(statecategories[i], occurrences, 'Scatter Plot', state, 'Occurrence')
-#     i = i + 1
-
+#Plot Year vs Length
+plotScatter(df['Year'], df['Length'], 'Scatter Plot', 'Year', 'Length')
 
 #-------------------------------------------- Multiple Linear Regression --------------------------------------------#
-X = df[df.columns.difference(['Occurrence'])] # here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
-Y = df['Occurrence']
+X = df[df.columns.difference(['Length'])] # here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
+Y = df['Length']
 #
 # # with sklearn
 regr = linear_model.LinearRegression()
@@ -102,21 +60,17 @@ print('Coefficients: \n', regr.coef_)
 
 #-------------------------------------------- Prediction --------------------------------------------#
 # prediction with sklearn
-newData = [101, 2030, 0]
-for i in range(0, len(statesDict)):
-    if i == 32:
-        newData.append(i)
-    else:
-        newData.append(0)
-print ('Predicted Occurrence: \n', regr.predict([newData]))
+
+newData = [0, 2030]
+print ('Predicted Occurrence Example (gender = 0, year = 2030): \n', regr.predict([newData]))
 
 
 #-------------------------------------------- Stats Models --------------------------------------------#
 # with statsmodels
-X = sm.add_constant(X) # adding a constant
-
-model = sm.OLS(Y, X).fit()
-predictions = model.predict(X)
-
-print_model = model.summary()
-print(print_model)
+# X = sm.add_constant(X) # adding a constant
+#
+# model = sm.OLS(Y, X).fit()
+# predictions = model.predict(X)
+#
+# print_model = model.summary()
+# print(print_model)
